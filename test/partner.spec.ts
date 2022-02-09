0be8c4b7274e5da61a753ae587b725376f0133c4
ShwethaAnne <99243634+ShwethaAnne@users.noreply.github.com>
2022-02-09 22:57:20 +0530

    add partner contact test

--- test/partner.spec.ts
+++ test/partner.spec.ts
@@ -14,7 +14,29 @@ beforeAll(async () => {
 
 afterAll(async () => await TestHelper.stopBroker(broker));
 
-test('partner  e2e', async () => {
+test('partner e2e', async () => {
+	let name: string = Utility.getRandomString(10);
+	const code: string = Utility.getRandomString(5);
+	const p: Partner = new Partner();
+	p.name = name;
+	p.code = code;
+	p.status = EntityStatusType.Active;
+
+	const savedpartner: Partner = await broker.call('v1.partner.insertPartner', p, opts);
+	await PartnerTestHelper.validatePartner(p);
+
+	savedpartner.name = Utility.getRandomString(10);
+	await broker.call('v1.partner.updatePartner', savedpartner, opts);
+	await PartnerTestHelper.validatePartner(savedpartner);
+
+	await broker.call('v1.partner.deletePartner', { id: savedpartner.id }, opts);
+	const allpartners: Partner[] = await broker.call('v1.partner.getPartners', {}, opts);
+	const pf = allpartners.filter((e) => e.code === p.code);
+
+	expect(pf).toBeArrayOfTypeOfLength(Partner, 0);
+});
+
+test('partner contact e2e', async () => {
 	let name: string = Utility.getRandomString(10);
 	const code: string = Utility.getRandomString(5);
 	const p: Partner = new Partner();
